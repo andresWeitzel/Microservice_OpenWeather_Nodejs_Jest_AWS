6ff25b66346370ce91a667fd7ff91ad65bb4f6ed
Andrés Weitzel <andres96energy@hotmail.com>
2023-11-07 23:11:53 -0300

    Added test

--- src/test/helpers/axios/request/get.test.js
+++ src/test/helpers/axios/request/get.test.js
@@ -12,28 +12,39 @@ const AXIOS_CONFIG = {
 describe("sendGetRequestTest Unit Test", () => {
   test("Should return an object or string for an http request", async () => {
     let axiosResponse = await sendGetRequest(GOOGLE_URL, AXIOS_CONFIG);
-    expect(
+    await expect(
       typeof axiosResponse == "string" || typeof axiosResponse == "object"
     ).toBe(true);
   });
 
-  test("Should be return null or undefined if no argument is passed to the function", async () => {
+  test("Should return null or undefined if no argument is passed to the function", async () => {
     let axiosResponse = await sendGetRequest();
-    expect(axiosResponse == (null || undefined)).toBe(true);
+    await expect(axiosResponse == (null || undefined)).toBe(true);
+  });
+
+  test("Should return error if no argument is passed to the function", async () => {
+    let axiosResponse = await sendGetRequest();
+    await expect(()=>axiosResponse).not.toThrow(Error);
   });
 
   test("Should return an object or string if only the url is passed to the function", async () => {
     let axiosResponse = await sendGetRequest(GOOGLE_URL, null);
-    expect(
+    await expect(
       typeof axiosResponse == "string" || typeof axiosResponse == "object"
     ).toBe(true);
   });
 
   test("Should return an object or string if only the url and config is passed to the function", async () => {
     let axiosResponse = await sendGetRequest(GOOGLE_URL, AXIOS_CONFIG);
-    expect(
+    await expect(
       typeof axiosResponse == "string" || typeof axiosResponse == "object"
     ).toBe(true);
+  });
+
+  test("Should return null or undefined with an invalid url ", async () => {
+    let INVALID_URL = "hff://zzz.com.ar"
+    let axiosResponse = await sendGetRequest(INVALID_URL);
+    await expect(axiosResponse == (null || undefined)).toBe(true);
   });
 
 });

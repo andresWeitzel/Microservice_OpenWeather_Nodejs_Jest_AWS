9bf406ca4bcfb9c8e8d9e2d04026564e347edf39
Andrés Weitzel <andres96energy@hotmail.com>
2023-11-09 23:42:35 -0300

    Updated tests

--- src/test/helpers/axios/request/get.test.js
+++ src/test/helpers/axios/request/get.test.js
@@ -2,6 +2,7 @@
 //Helpers
 const { sendGetRequest } = require("../../../../helpers/axios/request/get");
 //const
+const ERROR_MESSAGE = "error";
 const GOOGLE_URL = "https://www.google.com/";
 const AXIOS_CONFIG = {
   headers: {
@@ -10,28 +11,32 @@ const AXIOS_CONFIG = {
 };
 //vars
 let msg;
-let separator = "--------------------";
 
-describe("sendGetRequestTest Unit Test", () => {
+describe("- sendGetRequestTest helper (Unit Test)", () => {
   msg =
-    "- sendGetRequestTest Unit Test. Should return an object or string for an http request.";
-  console.log(separator);
-  console.log(msg);
-  test(msg, async () => {
+    "- Should return an object or string for an http request.";
+  it(msg, async () => {
     let axiosResponse = await sendGetRequest(GOOGLE_URL, AXIOS_CONFIG);
-    await expect(
+    expect(
       typeof axiosResponse == "string" || typeof axiosResponse == "object"
     ).toBe(true);
   });
 
   msg =
-    "- sendGetRequestTest Unit Test. Should return a string value with the massage of the error or exception if no argument is passed to the function";
-  console.log(separator);
-  console.log(msg);
-  test(msg, async () => {
+    "- Should return a string value with the message of the error or exception if no argument is passed to the function";
+  it(msg, async () => {
     let axiosResponse = await sendGetRequest();
-    await expect(typeof axiosResponse == "string").toBe(true);
+    expect(typeof axiosResponse == "string").toBe(true);
   });
+
+  msg =
+  "- It should return an error message if no argument is passed to the function and it should match the word ERROR.";
+it(msg, async () => {
+  let axiosResponse = await sendGetRequest();
+  expect(typeof axiosResponse == "string").toBe(true);
+  axiosResponse = axiosResponse.toLowerCase();
+  expect(axiosResponse).toMatch(ERROR_MESSAGE);
+});
 
   // test("Should return error if no argument is passed to the function", async () => {
   //   let axiosResponse = await sendGetRequest();

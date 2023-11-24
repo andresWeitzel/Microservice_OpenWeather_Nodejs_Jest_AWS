8d2c3d29e4a2a380a70655cc7bb7a524f2774d4d
Andrés Weitzel <andres96energy@hotmail.com>
2023-11-24 18:46:57 -0300

    Added helpers for fs

--- src/controllers/weather-condition/get.js
+++ src/controllers/weather-condition/get.js
@@ -2,6 +2,7 @@
 //helpers
 const { sendGetRequest } = require("../../helpers/axios/request/get");
 const { statusCode } = require("../../helpers/enums/http/status-code");
+const { createJson } = require("../../helpers/file-system/create-json");
 const { bodyResponse } = require("../../helpers/http/body-response");
 //const
 const API_WEATHER_URL_BASE = process.env.API_WEATHER_URL_BASE;
@@ -9,10 +10,13 @@ const API_KEY = process.env.API_KEY;
 const OK_CODE = statusCode.OK;
 const BAD_REQUEST_CODE = statusCode.BAD_REQUEST;
 //vars
+let filePathWeatherCondition =
+  "../../../data/json/weather-condition/weather-condition-data.json";
 let eventPathParams;
 let countryParam;
 let axiosConfig;
 let axiosResponse;
+let jsonData;
 
 module.exports.handler = async (event) => {
   try {
@@ -31,13 +35,16 @@ module.exports.handler = async (event) => {
 
     axiosResponse = await sendGetRequest(URL, null, axiosConfig);
 
-    if (axiosResponse != (null && undefined)) {
-      return await bodyResponse(OK_CODE, axiosResponse);
+    if (axiosResponse == (null || undefined)) {
+      return await bodyResponse(
+        BAD_REQUEST_CODE,
+        `Data could not be obtained by country ${countryParam}`
+      );
     }
-    return await bodyResponse(
-      BAD_REQUEST_CODE,
-      `Data could not be obtained by country ${countryParam}`
-    );
+
+    jsonData = await createJson(filePathWeatherCondition, axiosResponse);
+
+    return await bodyResponse(OK_CODE, axiosResponse);
   } catch (error) {
     console.log(error);
   }
